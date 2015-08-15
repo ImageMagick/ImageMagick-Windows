795cf36c4cf47b9faca527e9c101654da9dc74f3
dirk <dirk@git.imagemagick.org>
2015-08-15 14:45:58 +0200

    Only install the necessary version of Strawberryperl.

--- AppVeyor/Install.cmd
+++ AppVeyor/Install.cmd
@@ -2,10 +2,9 @@
 
 cd ..
 call CloneRepositories.cmd https://github.com/ImageMagick shallow
+
 if not exist "C:\Program Files (x86)\Inno Setup 5" choco install innosetup -y -version 5.5.2
-if exist "C:\Strawberry-x86" goto done
-choco install strawberryperl -y -x86 -version 5.20.1.1
-move C:\Strawberry C:\Strawberry-x86
-choco install strawberryperl -y -force -version 5.20.1.1
-move C:\Strawberry C:\Strawberry-x64
-:done
+
+set OPTION=
+if "%1" == "x86" SET OPTION=-x86
+choco install strawberryperl -y %OPTION% -version 5.20.1.1
