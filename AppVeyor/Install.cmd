5565be026719823bf88f1d6e32df5d0774099b9a
dirk <dirk@git.imagemagick.org>
2016-06-26 15:50:57 +0200

    Enabled the AppVeyor proxy and fixed checking of the errorlevel in the batch files.

--- AppVeyor/Install.cmd
+++ AppVeyor/Install.cmd
@@ -4,7 +4,9 @@ cd ..
 call CloneRepositories.cmd https://github.com/ImageMagick shallow
 
 if not exist "C:\Program Files (x86)\Inno Setup 5" choco install innosetup -y -version 5.5.2
+if %errorlevel% neq 0 exit /b %errorlevel%
 
 set OPTION=
 if "%1" == "x86" SET OPTION=-x86
 choco install strawberryperl -y %OPTION% -version 5.20.1.1
+if %errorlevel% neq 0 exit /b %errorlevel%
