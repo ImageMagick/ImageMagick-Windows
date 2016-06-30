6be40085eba810279ab7c8805c570f8817ca0b80
dirk <dirk@git.imagemagick.org>
2016-07-01 00:56:37 +0200

    Fixed setting the directory before executing the install scripts.

--- AppVeyor/Install.cmd
+++ AppVeyor/Install.cmd
@@ -8,18 +8,21 @@ if exist "C:\InnoSetup" goto strawberry
 
 mkdir %INNOSETUP%
 cd %INNOSETUP%
+echo Downloading innosetup-5.5.9-unicode.exe
 appveyor DownloadFile https://github.com/ImageMagick/ImageMagick-Windows/releases/download/20160630/innosetup-5.5.9-unicode.exe
 cd ..
 
 :strawberry
 
+if "%1" == "x64" goto x64
+
 set STRAWBERRYX86=C:\Strawberry-x86
-if exist %STRAWBERRYX86% goto x64
+if exist %STRAWBERRYX86% goto install
 
 mkdir %STRAWBERRYX86%
 cd %STRAWBERRYX86%
+echo Downloading strawberry-perl-5.20.1.1-32bit.msi
 appveyor DownloadFile https://github.com/ImageMagick/ImageMagick-Windows/releases/download/20160630/strawberry-perl-5.20.1.1-32bit.msi
-cd ..
 
 :x64
 
@@ -28,11 +31,12 @@ if exist %STRAWBERRY64% goto install
 
 mkdir %STRAWBERRY64%
 cd %STRAWBERRY64%
+echo Downloading strawberry-perl-5.20.1.1-64bit.msi
 appveyor DownloadFile https://github.com/ImageMagick/ImageMagick-Windows/releases/download/20160630/strawberry-perl-5.20.1.1-64bit.msi
-cd ..
 
 :install
 
+cd C:\ImageMagick\AppVeyor
 powershell -ExecutionPolicy Unrestricted .\Install-InnoSetup.ps1
 if "%1" == "x86" powershell -ExecutionPolicy Unrestricted .\Install-StrawberryPerl-x86.ps1
 if "%1" == "x64" powershell -ExecutionPolicy Unrestricted .\Install-StrawberryPerl-x64.ps1
