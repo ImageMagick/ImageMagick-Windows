b4c74e4d85467b1c8239ec9e2380c08949fc8938
Dirk Lemstra <dirk@lemstra.org>
2023-06-14 08:57:30 +0200

    Allow multiple release-info files in a project.

--- CheckReleases.sh
+++ CheckReleases.sh
@@ -2,10 +2,10 @@
 
 check_release()
 {
-  current=$(grep "DELEGATE_VERSION_NUM" "$1/ImageMagick/ImageMagick.version.h" | cut -d " " -f 7)
+  current=$(grep "DELEGATE_VERSION_NUM" "$(dirname $1)/ImageMagick.version.h" | cut -d " " -f 7)
   current=${current//,/.}
 
-  release_url=$(head -n 1 "$1/ImageMagick/release-info")
+  release_url=$(head -n 1 $1)
   format=$(echo $release_url | cut -d " " -f 2)
 
   release_url=$(echo $release_url | cut -d " " -f 1)
@@ -14,7 +14,7 @@ check_release()
     data=$(echo $data | base64 --decode)
   fi
 
-  regex=$(tail -n 1 "$1/ImageMagick/release-info")
+  regex=$(tail -n 1 $1)
   if [[ $data =~ $regex ]]; then
     latest=${BASH_REMATCH[1]}
     dot_count=$(echo $latest | grep -o "\." | wc -l)
@@ -25,23 +25,20 @@ check_release()
     latest="unable to load version"
   fi
 
+  project=$(basename $(dirname $(dirname $1)))
+
   if [ "$current" = "$latest" ]; then
     return 0
   fi
 
-  echo "$1${current} => ${latest} (${release_url})"
+  echo "$project ${current} => ${latest} (${release_url})"
   return 1
 }
 
 exit_code=0
 
-for d in */ ; do
-  if [ -f "$d/ImageMagick/release-info" ]; then
-    check_release $d
-    if [ $? != 0 ]; then
-      exit_code=1
-    fi
-  fi
+for f in $(find . -name "release-info"); do
+  check_release $f
 done
 
 exit $exit_code
